808c253ab3e602c377b0702fe2b944f4f9678053
Cabdifataax <thejufo@gmail.com>
2023-10-03 09:26:26 +0300

    Initial Commit

---
+++ RSS Reader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RSS Reader
+//
+//  Created by Cabdifataax Cabdilaahi on 03/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RSS Reader/RSS_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RSS_ReaderApp.swift
+//  RSS Reader
+//
+//  Created by Cabdifataax Cabdilaahi on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RSS_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
